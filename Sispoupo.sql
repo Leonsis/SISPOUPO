9801922d7b349d570a2407b5860cf7593cb72a79
Leonsis <caiolenni@gmail.com>
2026-07-24 00:09:06 -0300

    Aprimoramento do script SQL

--- Sispoupo.sql
+++ Sispoupo.sql
@@ -2,20 +2,49 @@ CREATE DATABASE SisPoupo;
 USE SisPoupo;
 
 DROP TABLE IF EXISTS users;
-
 CREATE TABLE users (
     id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nome_usuario VARCHAR(100) NOT NULL UNIQUE,
     nome VARCHAR(100) NOT NULL,
-    tipo_usuario VARCHAR(50) NOT NULL,
+    tipo_usuario VARCHAR(50) NOT NULL,  -- USUARIO_PADRAO | USUARIO_ADMIN | USUARIO_EMPRESA
     cpf_cnpj VARCHAR(14) NOT NULL UNIQUE,
     telefone VARCHAR(20) NOT NULL,
     email VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    situacao_cadastral TINYINT NOT NULL DEFAULT 1,
+    situacao_cadastral TINYINT NOT NULL DEFAULT 1, -- 1 = Ativo, 0 = Inativo
     created_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 -- Senha: JohnDoe
 INSERT INTO users (nome_usuario, nome, tipo_usuario, cpf_cnpj, telefone, email, password, situacao_cadastral) 
 VALUES ('SisPoupo', 'Usuario Teste','USUARIO_PADRAO', '00000000000','00000000', 'sispoupo@gmail.com', '$2y$10$jSAr/RwmjhwioDlJErOk9OQEO7huLz9O6Iuf/udyGbHPiTNuB3Iuy', 1);
+
+DROP TABLE IF EXISTS cartao_credito;
+CREATE TABLE cartao_credito (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    nome_cartao VARCHAR(100) NOT NULL,
+    limite_credito DECIMAL(10, 2) NOT NULL,
+    dia_vencimento INT NOT NULL,
+    created_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS movimentacao_financeira;
+CREATE TABLE movimentacao_financeira (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    descricao VARCHAR(255) NOT NULL,
+    tipo_movimentacao VARCHAR(100) NOT NULL, -- DESPESA ou RECEITA
+    valor DECIMAL(10, 2) NOT NULL,
+    classificacao_financeira VARCHAR(14) NOT NULL, -- FIXA, VARIAVEL
+    status_pagamento VARCHAR(20) NOT NULL, -- PAGO, NAO_PAGO,  pendente, atrasado
+    forma_pagamento VARCHAR(20) NOT NULL, -- DINHEIRO, CARTAO_CREDITO, CARTAO_DEBITO, PIX, TRANSFERENCIA_BANCARIA, boleto, cheque
+    quantidade_parcelas INT UNSIGNED NULL, -- Número de parcelas, se aplicável
+    cartao_credito_id BIGINT UNSIGNED NULL, -- Referência para o cartão de crédito, se aplicável
+    FOREIGN KEY (cartao_credito_id) REFERENCES cartao_credito(id) ON DELETE SET NULL,
+    created_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
